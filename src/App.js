ef98012d17c94d8ad78812ab4dd86701dabbc2db
jokkejok123 <47396357+jokkejok123@users.noreply.github.com>
2019-10-21 11:53:26 +0200

    Added boilerplate and favicon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './css/main.css';
 
 function App() {
   return (
     <div>
       <h1>Paradis</h1>
+      <p>Det bedste placeholder content du nogensinde har set</p>
     </div>
   );
 }
