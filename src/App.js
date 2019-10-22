40b4f649b221544683493b911d7ab45e6edcd1e1
jokkejok123 <47396357+jokkejok123@users.noreply.github.com>
2019-10-22 13:43:04 +0200

    Added hero-image. Adjusted toolbar colors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './css/main.css';
 import Toolbar from './components/Toolbar/Toolbar';
+import HeroImg from './assets/paradis-hero.jpg';
 
 function App() {
   return (
     <div>
       <Toolbar />
-      <h1>Paradis</h1>
-      <p>Det bedste placeholder content du nogensinde har set</p>
+      <div className="site-content">
+        <div className="hero-img">
+          <img src={HeroImg} alt="Paradis pige billede"/>
+        </div>
+        <div className="container">
+          <div className="row">
+            <h1>Paradis</h1>
+            <p>Det bedste placeholder content du nogensinde har set</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
